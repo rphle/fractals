833e5278ac298969a6461ca7963544e79fd9e999
Dr-Lego <Dr_Lego@ist-einmalig.de>
2025-11-13 18:46:38 +0100

    Fix workflow

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-# Build Rust WASM
-cd rust/fractal
-wasm-pack build --target web --dev --out-dir ../../ui/static/fractal
-
 # Build Svelte UI
-cd ../../ui
-tree static
+cd ui
 npm install
 npm run build
+
+# Build Rust WASM
+cd ../rust/fractal
+wasm-pack build --target web --dev --out-dir ../../ui/static/fractal
+cp -r ../../ui/static/fractal ../../ui/build/fractal
