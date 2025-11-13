5198672668506971af996e94dcfbc793d3708b90
Dr-Lego <Dr_Lego@ist-einmalig.de>
2025-11-13 19:11:25 +0100

    remove gitignore from build dir

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
+# Build Rust WASM
+cd rust/fractal
+wasm-pack build --target web --dev --out-dir ../../ui/static/fractal
+rm ../../ui/static/fractal/.gitignore
+
 # Build Svelte UI
-cd ui
+cd ../../ui
 npm install
 npm run build
-
-# Build Rust WASM
-cd ../rust/fractal
-wasm-pack build --target web --dev --out-dir ../../ui/static/fractal
-cp -r ../../ui/static/fractal ../../ui/build/fractal
