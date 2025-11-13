2a6fd18b50cb8b0fe1ea30de336b0f3a7e8a8096
Dr-Lego <Dr_Lego@ist-einmalig.de>
2025-11-13 19:31:25 +0100

    final fix

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,6 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 
+const isPages = Boolean(process.env.IS_PAGES);
+
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+  kit: {
+    adapter: adapter(), paths: {
+      base: isPages ? '/fractals' : ''
+    }
+  }
+};
 
 export default config;
